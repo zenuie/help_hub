be9de90d75dc6946a8fb622c2cad23adbe85e88e
jerry.liu <jerry.liu@uancare.com>
2025-09-27 13:05:29 +0800

    [Demo] 救災協助平台

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import Tasks from './pages/Tasks'
+import NeedForm from './pages/NeedForm'
+import MapPage from './pages/MapPage'
+import './App.css'
+import { setupForegroundSync } from './lib/sync'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  React.useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+    }
+    setupForegroundSync()
+  }, [])
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Tasks />} />
+        <Route path="/need" element={<NeedForm />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+createRoot(document.getElementById('root')!).render(<App />)
