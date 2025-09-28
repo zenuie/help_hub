7c366e76a2fb846751ec2f3c1fd26afac58368d8
jerry.liu <jerry.liu@uancare.com>
2025-09-28 13:56:27 +0800

    [Update] build

--- build/sw.js
+++ build/sw.js
@@ -1,21 +1,12 @@
 // public/sw.js
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('help_hub-shell').then((cache) =>
-      cache.addAll(['/', '/index.html', '/manifest.webmanifest'])
-    )
-  )
-})
-self.addEventListener('activate', () => self.clients.claim())
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url)
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(event.request).then((res) => res || fetch(event.request))
-    )
-    return
-  }
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  )
-})
+self.addEventListener('install', event => {
+  // 立即啟用
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  // 控制所有 client
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  // 預設直通網路
+});
